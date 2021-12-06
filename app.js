76d73e242497649df6a25d8d016c6757832c8521
Boris <bobabonanadz@mail.ru>
2021-12-07 01:43:05 +0300

    Create USER_CONNECT

--- app.js
+++ app.js
@@ -141,6 +141,7 @@ io.on('connection', (socket) => {
                     chatId,
                     username,
                 })
+                io.sockets.emit(SocketEmitters.USER_DISCONNECT)
                 /** For current socket */
                 socket.emit(SocketEmitters.USER_REFRESH_AFTER_LEAVE_CHAT)
             } catch (error) {
