429518e97cbacd64aadfc639f2e6d80ff217f187
Boris <bobabonanadz@mail.ru>
2021-12-07 01:06:39 +0300

    Create USER_DISCONNECT

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ io.on('connection', (socket) => {
         chats.forEach((chat) => {
             io.in(chat).emit(SocketEmitters.USER_OFFLINE, { userId, username })
         })
-        io.sokets.emit(SocketEmitters.USER_DISCONNECT)
+        io.sockets.emit(SocketEmitters.USER_DISCONNECT)
         socket.disconnect() // DISCONNECT SOCKET
         console.log('disconnect: ' + username)
     })
