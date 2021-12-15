5dc3dcb3fb54c2ec4701c39a795dae245d48f07a
Boris <bobabonanadz@mail.ru>
2021-12-15 18:21:11 +0300

    Change host client name

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'https://boris1979509.github.io/chat-client' /**'http://192.168.0.6:8080'*/,
+        origin: [
+            'https://boris1979509.github.io/chat-client',
+            'http://192.168.0.6:8080',
+        ],
         methods: ['GET', 'POST'],
     },
 })
