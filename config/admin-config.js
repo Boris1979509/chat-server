ca8ab3ad30348f1c0e0d6bee888d1d25b5172d3d
Boris <bobabonanadz@mail.ru>
2022-03-20 00:20:54 +0300

    Add env vars firebase admin

--- config/admin-config.js
+++ config/admin-config.js
@@ -1,8 +1,8 @@
-module.exports = JSON.stringify({
+module.exports = {
     type: 'service_account',
     project_id: 'chat-app-cfaa8',
     private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_ID,
-    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email:
         'firebase-adminsdk-zeefi@chat-app-cfaa8.iam.gserviceaccount.com',
     client_id: '113971831742200178050',
@@ -11,4 +11,4 @@ module.exports = JSON.stringify({
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_x509_cert_url:
         'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zeefi%40chat-app-cfaa8.iam.gserviceaccount.com',
-})
+}
